0deb514901178e6002be8873190b534a0bec8966
Siddharth Ashok <siddharthashokg@gmail.com>
2017-04-12 11:56:53 +0530

    cleanup

--- typeitlocal.php
+++ typeitlocal.php
@@ -14,27 +14,9 @@ defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 function say_hello(){
 echo 'Hello world. This is my first wordpress plugin';
 
-?>
-
-<script type="text/javascript">
-
-//var plugins_folder = '<?php echo plugin_basename( __FILE__ ); ?>';
-var plugins_folder = '<?php echo plugin_dir_url( __FILE__ ); ?>';
-
-//plugins_folder = plugins_folder.replace('typeitlocal.php','');
-
-var plugins_url = plugins_folder;
-
-console.log(plugins_folder);
-console.log(plugins_url);
-
-
-</script>
-<?php
-
+var plugins_url = '<?php echo plugin_dir_url( __FILE__ ); ?>';
 
 wp_enqueue_style('stylsheet', plugins_url( '/css/jquery.ime.css', __FILE__ ) );
-
 
 wp_enqueue_script( 'rangy-core', plugins_url('/libs/rangy/rangy-core.js',__FILE__), array(), '1.0.0', true );
 wp_enqueue_script( 'jquery_ime', plugins_url( '/src/jquery.ime.js', __FILE__ ), array(), '1.0.0', true );
